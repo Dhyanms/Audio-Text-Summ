a1b5555c87c3ec51fb71fbd74c6bf694a33b3263
Dhyanms <dhyanms@gmail.com>
2024-12-21 21:51:09 +0530

    Prototype

--- Final.py
+++ Final.py
@@ -8,9 +8,10 @@ from reportlab.pdfgen import canvas
 from moviepy.editor import AudioFileClip
 import tempfile
 import os
+from dontenv import load_dotenv
 
 # Set your Hugging Face API token here (ensure this is kept secure)
-hf_token = "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"
+hf_token = os.getenv('href')
 
 # Custom CSS
 st.markdown(
